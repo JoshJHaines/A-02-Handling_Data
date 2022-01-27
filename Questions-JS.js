82f7b8bc0815aa7128968136e35907630d820a7f
JoshuaJHaines <joshjhaines3@gmail.com>
2022-01-26 20:58:23 -0500

    my brain is dead

--- Questions-JS.js
+++ Questions-JS.js
@@ -42,4 +42,32 @@ console.log(map1);
 // of the people who have pizza as one of their favorite foods.
 // example: ['Peter', 'Mary']
 
+const reduce1 = userData.reduce((accumulator, user) => {
+  return [...accumulator, user.name]
+},
+  []
+)
+
+console.log(reduce1)
+
 // 7. Show us an example of a switch statement being used
+
+switch ("burgers") {
+	case "pizza":
+		console.log("this food is not healthy");
+		break;
+
+	case "tacos":
+		console.log("this food is only available on Tuesday");
+		break;
+
+	case "fried chicken":
+		console.log("this food is finger lickin");
+		break;
+	case "burgers":
+		console.log("pure American happiness");
+		break;
+
+	default:
+		console.log("we dont know this food");
+}
